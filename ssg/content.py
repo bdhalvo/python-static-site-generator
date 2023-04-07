613f3dd6d688e9126bdf306e9957d9a9a636642f
Brent Halvorson <halvorsb@wwt.com>
2023-04-07 11:36:55 -0600

    main - crorect an error in regex string assigned to __delimiter - bdh

--- ssg/content.py
+++ ssg/content.py
@@ -5,7 +5,8 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimeter = r"^(?:-|\+){3}\s*$"
+  # __delimeter = r"^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
 

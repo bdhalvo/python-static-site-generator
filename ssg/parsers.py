49e87c8dc1fddca981205420f15690808d7686a8
Brent Halvorson <halvorsb@wwt.com>
2023-04-07 10:07:22 -0600

    implements parser class for module2 - bdh

---
+++ ssg/parsers.py
@@ -0,0 +1,40 @@
+from typing import List
+from pathlib import Path
+import shutil
+
+
+class Parser():
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+  
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+
+    def read(self, path: Path):
+        with open(path, mode='rt', encoding='utf-8') as file:
+            return file.read()
+
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = self.dest / self.path.with_suffix.name(ext)
+
+        with open(full_path, mode='wt', encoding='utf-8') as file:
+            file.write(content)
+
+
+    def copy(self, path, source, dest):
+        shutil.copy(self.path, self.dest / path.relative_to(self.source))
+
+
+class ResourceParser(Parser):
+    def __init__(self):
+        super().__init__()
+        self.extensions = ['.jpg', '.png', '.gif', '.css', '.html']
+
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path, source, dest)

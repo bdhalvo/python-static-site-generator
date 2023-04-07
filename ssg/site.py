0cd0206675ba9fbc27205a8de8c6bcc94dd3ba31
Brent Halvorson <halvorsb@wwt.com>
2023-04-07 08:29:06 -0600

    main - add self param to build method - bdh

--- ssg/site.py
+++ ssg/site.py
@@ -13,9 +13,9 @@ class Site():
     directory.mkdir(parents=True, exist_ok=True)
 
 
-  def build():
+  def build(self):
     self.dest.mkdir(parents=True, exist_ok=True)
 
     for path in self.source.rglob("*"):
       if path.is_dir():
-        create_dir(path)
+        self.create_dir(path)

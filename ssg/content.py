39121a9d45d07eb6be94181cf56f634c8891e2b3
Brent Halvorson <halvorsb@wwt.com>
2023-04-07 11:38:46 -0600

    main - learned how to spell delimiter - bdh

--- ssg/content.py
+++ ssg/content.py
@@ -7,7 +7,7 @@ from collections.abc import Mapping
 class Content(Mapping):
   # __delimeter = r"^(?:-|\+){3}\s*$"
     __delimiter = r"^(?:-|\+){3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __regex = re.compile(__delimiter, re.MULTILINE)
 
 
     @classmethod
